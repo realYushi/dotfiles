8d9dcc3c03294116f9b0ceed7b536672486f730f
Yushi Cui <realyushi@gmail.com>
2026-06-22 14:22:25 +1200

    Update dotfiles

--- tmux/bin/ram-percentage.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-exec "$HOME/.config/tmux/plugins/tmux-cpu/scripts/ram_percentage.sh" "$@"
--- tmux/bin/treemux-python.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-exec "$HOME/.local/share/treemux-venv/bin/python3" "$@"
